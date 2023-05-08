b14dc8ea97140ab88a9b2c03d623087e9a9818ee
totarochristian <totaro.christian.18@gmail.com>
2023-05-08 15:15:57 +0200

    Update the quests.php file to manage the post request

--- API/quests.php
+++ API/quests.php
@@ -4,7 +4,13 @@
   //Convert the data content to an associative array
   $dataArray = json_decode($dataContent, true);
 
-  // ... do other things ...
+  //If receive post request with the field quests
+  if(isset($_POST["quests"])){
+    //Update the current data array
+    $dataArray = $_POST["quests"];
+    //Update the file containing the data
+    file_put_contents("data.json", json_encode($dataArray));
+  }
 
   //Set the content type of the data retrieved to the client
   header("Content-Type: application/json");

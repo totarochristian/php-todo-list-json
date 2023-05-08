2b90e140b540dd69b5319b3565439dc4c64dc97b
totarochristian <totaro.christian.18@gmail.com>
2023-05-08 15:02:07 +0200

    Add data.json file and update quests.php file

--- API/quests.php
+++ API/quests.php
@@ -1,9 +1,9 @@
 <?php
   //Get data content from the file
-  $dataContent = file_get_contents("./data.json");
+  $dataContent = file_get_contents("data.json");
   //Convert the data content to an associative array
   $dataArray = json_decode($dataContent, true);
-  
+
   // ... do other things ...
 
   //Set the content type of the data retrieved to the client

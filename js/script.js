5aa99c8323d4b71121102303af49a379c4cbf91a
totarochristian <totaro.christian.18@gmail.com>
2023-05-08 15:42:09 +0200

    Define the UpdateFile method, call it inside the methods AddQuests, SetQuestResult and RemoveQuest + update the logic in add quests to decide to push or set the array + update the load data method

--- js/script.js
+++ js/script.js
@@ -13,21 +13,27 @@ createApp({
         AddQuests(){
             if(newQuest.value){
                 const obj = { id: 'quest_' + (this.lastQuestId += 1), toDo: newQuest.value, done: 0 };
-                this.quests.push(obj);
+                if(this.quests)
+                    this.quests.push(obj);
+                else
+                    this.quests = [ obj ];
                 this.newQuest = '';
+                this.UpdateFile();
             }
         },
         SetQuestResult(index,value){
             this.quests[index].done = value;
+            this.UpdateFile();
         },
         RemoveQuest(index){
             this.quests.splice(index,1);
+            this.UpdateFile();
         },
         LoadData(){
-            axios.get(this.urlApi).then((res) => {
-                console.log(res);
-                this.quests = res.data;
-            });
+            axios.get(this.urlApi).then((res) => { this.quests = res.data; });
+        },
+        UpdateFile(){
+            axios.post(this.urlApi,{"quests": this.quests},{headers: {'Content-Type': 'multipart/form-data'}});
         }
     },
     mounted(){

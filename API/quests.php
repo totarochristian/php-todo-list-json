64bac84487f92ecbdec5d3426a27d4b27d22a190
totarochristian <totaro.christian.18@gmail.com>
2023-05-08 14:50:01 +0200

    Define the quests.php file, this file rapresent a basic API that will retrieve to the user the data contained in the data file

---
+++ API/quests.php
@@ -0,0 +1,12 @@
+<?php
+  //Get data content from the file
+  $dataContent = file_get_contents("./data.json");
+  //Convert the data content to an associative array
+  $dataArray = json_decode($dataContent, true);
+  
+  // ... do other things ...
+
+  //Set the content type of the data retrieved to the client
+  header("Content-Type: application/json");
+  //Print the array data in the page encoded in JS
+  echo json_encode($dataArray);
